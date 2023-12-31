83176dc27f4d9fff052b8a57c8b823b209c01e08
simransin <simransingh9876rajgir@gmail.com>
2023-12-31 23:14:18 +0530

    Creating separate js file

---
+++ script.js
@@ -0,0 +1,6 @@
+ //creating react heading
+ const heading = React.createElement("h1", {}, "Hello World from React!!");
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(heading);
