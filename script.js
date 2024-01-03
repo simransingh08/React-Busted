718f27bfbb0b1dd0ca0367975cafceefdcff5fe1
simransin <simransingh9876rajgir@gmail.com>
2024-01-03 23:31:08 +0530

    Creating html code through react

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// <div id = "parent">
+//    <div id = "child">
+//    <h1> I am h1 tag </h1>
+// </div>
+// ReactElement(Object) => HTML(Browser Understand)
+
+  
+const parent = React.createElement(
+   "div",
+   {id: "parent" },
+   React.createElement(
+      "div",{ id: "child" },
+      React.createElement("h1", {}, "I am h1 tag")
+   )
+);
+ 
+ 
  //creating react heading
  const heading = React.createElement("h1", { 
     id: "heading", xyz: "abc"
  }, "Hello World from React!!");
 
- console.log(heading)
+ console.log(heading);
+
  const root = ReactDOM.createRoot(document.getElementById("root"));
 
  root.render(heading);
